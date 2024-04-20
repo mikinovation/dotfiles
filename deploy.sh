e616b437f27836303d1ee884cc22fc3e3019cfa1
mikinovation <maneuver2472@gmail.com>
2024-04-20 10:31:48 +0900

    feat: add errorHandling

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,8 @@ link_tmux_config() {
 # Neovimの設定ファイルをdotfilesリポジトリからシンボリックリンクとして配置
 link_nvim_config() {
   echo "Linking neovim config..."
-  for file in  $(ls -a "${DOTFILES_DIR}"/config/nvim); do
+  for file in "${DOTFILES_DIR}"/config/nvim); do
+    [[ -f "$file" ]] || continue
     ln -snfv "$DOTFILES_DIR/config/nvim/$file" "$NVIM_CONFIG_DIR/$file"
   done
   ln -snfv "$DOTFILES_DIR/config/nvim/settings/coc-settings.json" "$NVIM_CONFIG_DIR/coc-settings.json"
