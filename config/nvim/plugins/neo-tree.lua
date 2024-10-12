cadda2964cae98ceeccae6c3aa501ac3b6ea9384
mikinovation <maneuver2472@gmail.com>
2024-10-12 15:14:32 +0800

    fix: change deploy command

--- config/nvim/plugins/neo-tree.lua
+++ config/nvim/plugins/neo-tree.lua
@@ -10,6 +10,11 @@ function neoTree.config()
 			"MunifTanjim/nui.nvim",
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
+		opts = {
+			filesystem = {
+				filtered_items = { visible = true, hide_dotfiles = false },
+			},
+		},
 	}
 end
 
