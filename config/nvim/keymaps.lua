89698f9cda9e683d30520b54558a13367a6f3995
mikinovation <maneuver2472@gmail.com>
2024-10-27 20:33:29 +0800

    feat: add diagnostic keymaps

--- config/nvim/keymaps.lua
+++ config/nvim/keymaps.lua
@@ -2,7 +2,8 @@ local keymap = vim.keymap.set
 
 -- keymap(mode, lhs, rhs, opts?)("n", "<Esc>", "<cmd>nohlsearch<CR>")
 keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
-keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+keymap("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostics in loclist" })
+keymap("n", "<leader>df", vim.diagnostic.open_float, { desc = "Open diagnostics in float" })
 keymap("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 keymap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 keymap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
