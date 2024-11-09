f0bc0602fefd22aa0edda3995668da68a09936a7
mikinovation <maneuver2472@gmail.com>
2024-11-09 08:06:39 +0800

    refactor: move neoscroll to its own file

---
+++ config/nvim/plugins/neoscroll.lua
@@ -0,0 +1,12 @@
+local neoscroll = {}
+
+function neoscroll.config()
+	return {
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup()
+		end,
+	}
+end
+
+return neoscroll
