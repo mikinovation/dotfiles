bf36c5591e97855c2396de6e5b5185b6574af508
mikinovation <maneuver2472@gmail.com>
2025-04-30 21:08:24 +0800

    feat: add octo

---
+++ config/nvim/plugins/octo.lua
@@ -0,0 +1,17 @@
+local octo = {}
+
+function octo.config()
+	return {
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup({})
+		end,
+	}
+end
+
+return octo
