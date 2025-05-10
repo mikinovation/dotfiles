245c9321d7c267dd9e063476135eed96924453d2
mikinovation <maneuver2472@gmail.com>
2025-05-10 13:56:10 +0800

    fix: improve TypeScript formatting with eslint_d
    - Change formatting event from BufWritePre to BufWritePost
    - Improve async formatting function to handle modified buffers
    - Use none-ls formatters directly and add debug mode
    - Organize code actions and diagnostics

--- config/nvim/plugins/none-ls.lua
+++ config/nvim/plugins/none-ls.lua
@@ -9,43 +9,63 @@ function noneLs.config()
 			require("plugins.plenary").config(),
 		},
 		config = function()
+			local async_formatting = function(bufnr)
+				bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+				vim.lsp.buf_request(
+					bufnr,
+					"textDocument/formatting",
+					vim.lsp.util.make_formatting_params({}),
+					function(err, res, ctx)
+						if err then
+							local err_msg = type(err) == "string" and err or err.message
+							-- you can modify the log message / level (or ignore it completely)
+							vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+							return
+						end
+
+						-- don't apply results if buffer is unloaded or has been modified
+						if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+							return
+						end
+
+						if res then
+							local client = vim.lsp.get_client_by_id(ctx.client_id)
+							vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+							vim.api.nvim_buf_call(bufnr, function()
+								vim.cmd("silent noautocmd update")
+							end)
+						end
+					end
+				)
+			end
+
 			local null_ls = require("null-ls")
+
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
+					require("none-ls.diagnostics.eslint_d"),
 					require("none-ls.formatting.eslint_d"),
+					require("none-ls.code_actions.eslint_d"),
 					null_ls.builtins.formatting.rustfmt,
-					null_ls.builtins.diagnostics.eslint_d,
 					null_ls.builtins.diagnostics.stylelint.with({
 						filetypes = { "css", "scss", "vue" },
 					}),
-
-					null_ls.builtins.code_actions.eslint_d,
 				},
-
+				debug = false,
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_create_autocmd("BufWritePre", {
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePost", {
+							group = augroup,
 							buffer = bufnr,
 							callback = function()
-								vim.lsp.buf.format({
-									bufnr = bufnr,
-									filter = function(lsp_client)
-										return lsp_client.name == "null-ls"
-									end,
-								})
+								async_formatting(bufnr)
 							end,
 						})
-
-						vim.keymap.set("n", "<leader>nf", function()
-							vim.lsp.buf.format({
-								bufnr = bufnr,
-								filter = function(lsp_client)
-									return lsp_client.name == "null-ls"
-								end,
-							})
-						end, { buffer = bufnr, desc = "Format current buffer with none-ls" })
 					end
 				end,
 			})
