d865990621a284209d9d79ac2394bc4ac955a45d
mikinovation <maneuver2472@gmail.com>
2025-05-24 23:49:45 +0800

    fix: remove version field from LuaSnip to use commit-based versioning
    - Remove version constraint to rely solely on commit hash
    - Ensures consistent dependency management with other plugins

--- config/nvim/plugins/luasnip.lua
+++ config/nvim/plugins/luasnip.lua
@@ -4,7 +4,6 @@ function luasnip.config()
 	return {
 		"L3MON4D3/LuaSnip",
 		commit = "03c8e67eb7293c404845b3982db895d59c0d1538",
-		version = "v2.*",
 		build = "make install_jsregexp",
 		dependencies = {
 			require("plugins.friendly-snippets").config(),
