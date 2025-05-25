e70f076e05a15f99a57c4b754210ac19017c31b8
mikinovation <maneuver2472@gmail.com>
2025-05-25 12:22:52 +0800

    chore: update plugin versions
    - Update dropbar.nvim to latest commit
    - Update telescope.nvim to latest commit and remove branch pin
    - Update lazy-lock.json with latest plugin versions

--- config/nvim/plugins/dropbar.lua
+++ config/nvim/plugins/dropbar.lua
@@ -3,7 +3,7 @@ local dropbar = {}
 function dropbar.config()
 	return {
 		"Bekaboo/dropbar.nvim",
-		commit = "cb7c17bb35fe8860d490dfd1d5c45fce40ecba26",
+		commit = "f7ecb0c3600ca1dc467c361e9af40f97289d7aad",
 		-- optional, but required for fuzzy finder support
 		dependencies = {
 			require("plugins.telescope-fzf-native").config(),
