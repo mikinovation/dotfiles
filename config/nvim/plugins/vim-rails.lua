f8d45e13159274a5d51300f55e838f248d656d88
mikinovation <maneuver2472@gmail.com>
2025-05-29 22:17:37 +0800

    chore: update vim rails to version manage

--- config/nvim/plugins/vim-rails.lua
+++ config/nvim/plugins/vim-rails.lua
@@ -3,8 +3,8 @@ local vimRails = {}
 function vimRails.config()
 	return {
 		"tpope/vim-rails",
-		-- renovate: datasource=git-refs depName=https://github.com/tpope/vim-rails
-		commit = "d3954dfe3946c9330dc91b4fbf79ccacb2c626c0",
+		-- renovate: datasource=github-releases depName=tpope/vim-rails
+		version = "5.3",
 	}
 end
 
