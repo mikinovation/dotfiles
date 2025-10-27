d068020cb8ed7200802e949bd4bab184e2ea86fd
mikinovation <maneuver2472@gmail.com>
2025-10-27 21:13:14 +0800

    feat: add typescript-go package to Node.js configuration

--- config/nix/configs/nodejs.nix
+++ config/nix/configs/nodejs.nix
@@ -4,16 +4,17 @@
   home.packages = with pkgs; [
     # Node.js LTS version (includes npm by default)
     nodejs_22
-    
+
     # Package managers
     yarn
     nodePackages.pnpm
-    
+
     # Development tools
     nodePackages.typescript
     nodePackages.typescript-language-server
     nodePackages.eslint
     nodePackages.prettier
+    typescript-go  # Go implementation of TypeScript
   ];
 
   # Environment variables for Node.js
