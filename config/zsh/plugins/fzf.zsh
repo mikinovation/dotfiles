4feaf1a33a3cc17d7a8be6bc6d78a73352424bda
mikinovation <maneuver2472@gmail.com>
2026-03-29 22:49:05 +0900

    fix: use portable fc -nrl and eval for cmi command execution
    - Replace `fc -ln 1 | tac` with `fc -nrl 1` to avoid GNU coreutils dependency
    - Replace `print -z` with `print -s` + `eval` to actually execute the selected command

--- config/zsh/plugins/fzf.zsh
+++ config/zsh/plugins/fzf.zsh
@@ -3,8 +3,9 @@
 # cmi: Select a command from history via fzf and execute it
 function cmi() {
   local cmd
-  cmd=$(fc -ln 1 | tac | awk '!seen[$0]++' | fzf --no-sort --layout=reverse --prompt="Select command: ")
+  cmd=$(fc -nrl 1 | awk '!seen[$0]++' | fzf --no-sort --layout=reverse --prompt="Select command: ")
   if [[ -n "$cmd" ]]; then
-    print -z "$cmd"
+    print -s -- "$cmd"
+    eval "$cmd"
   fi
 }
